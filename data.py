cdccd071bdc822f6ae80efe155411821da176e75
Sean Chu <chussean@gmail.com>
2019-05-03 16:38:59 -0400

    Basic Backend Functionality

--- data.py
+++ data.py
@@ -35,8 +35,50 @@ def getItems():
     mycursor.execute('SELECT * FROM Item NATURAL JOIN Inventory')
     result = mycursor.fetchall()
     return result
-
-#def insertShoppingCart(
-#addItem(1,'Cup',1.24,'Utensil','Mikes',3)
-getItems()
+#Given a ShoppingCart ID and a dictionary of ItemID:Quantity
+def insertShoppingCart(idnum, items):
+    global mycursor
+    prices = []
+    for x in items:
+        sql = 'SELECT Price FROM Item WHERE ItemID = (%s)'
+        itemnum = (x,)
+        mycursor.execute(sql,itemnum)
+        result = mycursor.fetchone()
+        price = result[0] * items[x]
+        prices.append(price)
+    i = 0
+    for y in prices:
+        sql = 'INSERT INTO ShoppingCart (TotalPrice,ShoppingCartID,ItemID,ItemQuantity) VALUES (%s,%s,%s,%s)'
+        key = (list)(items.keys())[i]
+        val = (y,idnum,key,items[key])
+        mycursor.execute(sql,val)
+    mydb.commit()
+#Given shoppingCart id gets a list of tuples of (TotalPrice,ShoppingCartID,ItemID,Quantity)
+def getShoppingCart(idnum):
+    global mycursor
+    sql = 'SELECT * FROM ShoppingCart WHERE ShoppingCartID = (%s)'
+    val = (idnum,)
+    mycursor.execute(sql,val)
+    result = mycursor.fetchall()
+    return result
+#Given Shoppingcart id, removes all entries with that id from shoppingCart, then updates Inventory
+def removeShoppingCart(idnum):
+    global mycursor
+    sql = 'SELECT ItemID,ItemQuantity FROM ShoppingCart WHERE ShoppingCartID = (%s)'
+    val = (idnum,)
+    mycursor.execute(sql,val)
+    purchased = mycursor.fetchall()
+    sql = 'DELETE FROM ShoppingCart WHERE ShoppingCartID = (%s)'
+    mycursor.execute(sql,val)
+    for x in purchased:
+        sql = 'SELECT Quantity FROM Inventory WHERE ItemID =(%s) '
+        val = (x[0],)
+        mycursor.execute(sql,val)
+        currQ = mycursor.fetchone()
+        newQ = currQ[0] - x[1]
+        sql = 'UPDATE Inventory SET Quantity = (%s) WHERE ItemID = (%s)'
+        val = (newQ,x[0])
+        mycursor.execute(sql,val)
+    mydb.commit()
+        
 
