e5075fa077101733871aebfae36b9927798d7eb9
Raymond Wu <rayw.u17@gmail.com>
2019-05-04 16:19:33 -0400

    More routes for main.py, more samples

--- main.py
+++ main.py
@@ -26,11 +26,38 @@ def listing():
     print(items)
     return items
 
+@app.route("/popular")
+def popular():
+    """
+    Gets items from database sorted by popularity
+    """
+    items = data.getItemRating()
+    print(items)
+    return items
+
+@app.route("/addcart")
+def addcart():
+    """
+    Adds items to shopping cart
+    """
+
 @app.route("/cart")
 def cart():
     """
     Gets items from shopping cart
     """
+    cart = getShoppingCart(1)
+    print(cart)
+    return cart
+
+@app.route("/review/<int:idnum>")
+def review(idnum):
+    """
+    Gets review of an item given id
+    """
+    reviews = data.getReview(idnum)
+    print(reviews)
+    return reviews
 
 if __name__ == "__main__":
     app.run(debug = True)
