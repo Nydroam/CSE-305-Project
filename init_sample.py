aa7dd719241eb467d48c4bf349ca3e444214e3d6
Raymond Wu <rayw.u17@gmail.com>
2019-05-04 16:57:09 -0400

    commenting for samples

--- init_sample.py
+++ init_sample.py
@@ -10,6 +10,7 @@ mydb = mysql.connector.connect(
     )
 mycursor = mydb.cursor()
 
+#itemid, name, price, type, seller, quantity
 data.addItem(111, "Toothpaste", 4.55, "Personal Care", "Colgate", 103)
 data.addItem(290, "Mouthwash", 6.57, "Personal Care", "Listerine", 247)
 data.addItem(424, "Pencil", 1.99, "Office Supplies", "Dixon", 2836)
@@ -26,6 +27,8 @@ data.addItem(1234, "16 GB USB 3.0 Flash Drive", 14.99, "Electronics", "SanDisk",
 data.addItem(1223, "64 GB USB 2.0 Flash Drive", 39.99, "Electronics", "SanDisk", 17683)
 data.addItem(1210, "32 GB USB 3.0 Flash Drive", 24.99, "Electronics", "Samsung", 5069)
 print(data.getItems())
+
+#rating, detailedreview, itemid, customerid, reviewid
 data.addReview(3,'test',456,1,1)
 data.addReview(4,'test',456,1,2)
 data.addReview(2,'test',304,1,3)
